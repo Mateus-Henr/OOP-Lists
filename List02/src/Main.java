b4d08c40bc6c8ecb68f517d37a72db68b95522fa
Mateus-Henr <mateush.study@gmail.com>
2022-09-20 10:52:10 -0300

    Modularize code

--- List02/src/Main.java
+++ List02/src/Main.java
@@ -1,4 +1,7 @@
+import model.Contrato;
+import model.FolhaDePagamento;
 import model.Funcionario;
+import model.TipoContrato;
 
 import java.time.LocalDate;
 
@@ -11,32 +14,32 @@ public class Main
         Funcionario pedro = new Funcionario("Maria", LocalDate.parse("1980-07-07"));
         Funcionario caio = new Funcionario("Maria", LocalDate.parse("1985-02-09"));
 
-        maria.contratar(Funcionario.TipoContrato.Efetivo, 1000.00, 1, LocalDate.parse("2016-02-09"));
-        joao.contratar(Funcionario.TipoContrato.Efetivo, 1905.00, 3, LocalDate.parse("2016-02-09"));
-        pedro.contratar(Funcionario.TipoContrato.Efetivo, 2900.00, 2, LocalDate.parse("2016-02-09"));
-        caio.contratar(Funcionario.TipoContrato.Efetivo, 5000.00, 0, LocalDate.parse("2016-02-09"));
+        Contrato padrao = new Contrato();
+        padrao.contratar(TipoContrato.Efetivo, 2000, 1, LocalDate.parse("1998-09-09"));
+
+        FolhaDePagamento folhaDePagamento = new FolhaDePagamento(maria, padrao);
 
         System.out.println("Funcionários:");
 
         System.out.println("\n1° Funcionário\n");
         System.out.println(maria);
         System.out.println();
-        maria.mostrarFolhaDePagamento(8, Funcionario.TipoContrato.Efetivo);
+        folhaDePagamento.mostrarFolhaDePagamento(8);
 
         System.out.println("\n\n\n2° Funcionário\n");
         System.out.println(joao);
         System.out.println();
-        joao.mostrarFolhaDePagamento(10, Funcionario.TipoContrato.Efetivo);
+        folhaDePagamento.mostrarFolhaDePagamento(10);
 
         System.out.println("\n\n\n3° Funcionário\n");
         System.out.println(pedro);
         System.out.println();
-        pedro.mostrarFolhaDePagamento(8, Funcionario.TipoContrato.Horista);
+        folhaDePagamento.mostrarFolhaDePagamento(8);
 
         System.out.println("\n\n\n4° Funcionário\n");
         System.out.println(caio);
         System.out.println();
-        caio.mostrarFolhaDePagamento(7, Funcionario.TipoContrato.Efetivo);
+        folhaDePagamento.mostrarFolhaDePagamento(7);
         System.out.println();
     }
 
--- List02/src/model/Funcionario.java
+++ List02/src/model/Funcionario.java
@@ -4,30 +4,13 @@ import java.time.LocalDate;
 
 public class Funcionario
 {
-    public enum TipoContrato
-    {
-        Efetivo, Horista
-    }
-
     private final String nome;
     private final LocalDate dataNascimento;
-    private TipoContrato tipoContrato;
-    private double salario;
-    private int filhosMenoresDeIdade;
-    private LocalDate dataAdmissao;
 
     public Funcionario(String nome, LocalDate dataNascimento)
     {
         this.nome = nome;
         this.dataNascimento = dataNascimento;
-    }
-
-    public void contratar(TipoContrato tipoContrato, double salario, int filhosMenorDeIdade, LocalDate dataAdmissao)
-    {
-        this.tipoContrato = tipoContrato;
-        this.salario = salario;
-        this.filhosMenoresDeIdade = filhosMenorDeIdade;
-        this.dataAdmissao = dataAdmissao;
     }
 
     public String getNome()
@@ -40,135 +23,16 @@ public class Funcionario
         return dataNascimento;
     }
 
-    public TipoContrato getTipoContrato()
-    {
-        return tipoContrato;
-    }
-
-    public void setTipoContrato(TipoContrato tipoContrato)
-    {
-        this.tipoContrato = tipoContrato;
-    }
-
-    public double getSalario()
-    {
-        return salario;
-    }
-
-    public void setSalario(double salario)
-    {
-        this.salario = salario;
-    }
-
-    public int getFilhosMenoresDeIdade()
-    {
-        return filhosMenoresDeIdade;
-    }
-
-    public void setFilhosMenoresDeIdade(int filhosMenoresDeIdade)
-    {
-        this.filhosMenoresDeIdade = filhosMenoresDeIdade;
-    }
-
-    public LocalDate getDataAdmissao()
-    {
-        return dataAdmissao;
-    }
-
-    public void setDataAdmissao(LocalDate dataAdmissao)
-    {
-        this.dataAdmissao = dataAdmissao;
-    }
-
-    public double calculaSalarioBruto(int horasTrabalhadas, TipoContrato tipoContrato)
-    {
-        if (tipoContrato == TipoContrato.Horista)
-        {
-            return horasTrabalhadas * salario;
-        }
-
-        return salario;
-    }
-
-    public double calculaDescontoINSS()
-    {
-        if (salario <= 1659.38)
-        {
-            return salario * 0.08;
-        }
-        else if (salario >= 1659.39 && salario <= 2765.66)
-        {
-            return salario * 0.09;
-        }
-        else if (salario >= 2765.67 && salario <= 5531.31)
-        {
-            return salario * 0.11;
-        }
-        else
-        {
-            return 5531.31 * 0.11;
-        }
-    }
-
-    public double calculaDescontoImpostoDeRenda()
-    {
-        if (salario <= 1903.98)
-        {
-            return 0;
-        }
-        else if (salario >= 1903.99 && salario <= 2826.65)
-        {
-            return (salario * 0.075) - 142.80;
-        }
-        else if (salario >= 2826.66 && salario <= 3751.05)
-        {
-            return (salario * 0.15) - 354.80;
-        }
-        else if (salario >= 3751.06 && salario <= 4664.68)
-        {
-            return (salario * 0.225) - 636.13;
-        }
-        else
-        {
-            return (salario * 0.275) - 869.36;
-        }
-    }
-
-    public double calculaSalarioLiquido()
-    {
-        return salario - calculaDescontoINSS() - calculaDescontoImpostoDeRenda();
-    }
-
-    public void mostrarFolhaDePagamento(int horasTrabalhadas, TipoContrato tipoContrato)
-    {
-        System.out.printf("""
-                        ------- Folha de Pagamento -------
-                        Salário Bruto: R$ %.2f
-                        Descontos:
-                        INSS: R$ %.2f
-                        Imposto de Renda: R$ %.2f
-                        Salário Liquído: R$ %.2f""",
-                calculaSalarioBruto(horasTrabalhadas, tipoContrato),
-                calculaDescontoINSS(),
-                calculaDescontoImpostoDeRenda(),
-                calculaSalarioLiquido());
-    }
-
     @Override
     public String toString()
     {
         return String.format("""
                         ------ Dados do Funcionário ------
                         Nome: %s
-                        Contrato: %s
-                        Salário: R$ %.2f
-                        Filhos Menores de Idade: %d
-                        Data de Admissão: %s""",
+                        Data de Nascimento: %s
+                        """,
                 nome,
-                tipoContrato,
-                salario,
-                filhosMenoresDeIdade,
-                dataAdmissao);
+                dataNascimento);
     }
 
 }
